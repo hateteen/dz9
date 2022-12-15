d40573ab52bdc1d09ce1348b7f11c62dc9c353ec
hateteen <116436850+hateteen@users.noreply.github.com>
2022-12-15 18:45:16 +0300

    z66

--- Program.cs
+++ Program.cs
@@ -21,3 +21,31 @@
             return Akkerman(m - 1, Akkerman(m, n - 1));
     }
 }
+
+void Zadacha66()
+{
+    Console.WriteLine("Введите начальное число M:");
+    int numberM = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите начальное число M:");
+    int numberN = int.Parse(Console.ReadLine());
+
+    ///Метод нахождения суммы натуральных элементов в промежутке от M до N
+    void GapNumberSum (int numberM, int numberN, int sum)
+    {
+        if (numberM > numberN) 
+        {
+            Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+            return;
+        }
+        sum = sum + (numberM++);
+        GapNumberSum(numberM, numberN, sum);
+    }
+
+    GapNumberSum(numberM, numberN, 0);
+}
+
+void Zadacha64()
+{
+
+}
